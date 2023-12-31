a967a2723c9e4e5d5a45ee715c1dc632ec27242d
Eric Hao <hhao99@gmail.com>
2023-12-31 12:35:23 -0600

    datatable

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dash_table
 # import plotly.express as px
-# import pandas as pd 
+import pandas as pd 
 
+url = './housing.parquet'
+df = pd.read_parquet(url)
+title = 'Canada Housing Statistics'
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children="Hello World"),
+    html.H1(children= title),
+    dash_table.DataTable(data=df.to_dict('records'),page_size=10)
 ]);
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
